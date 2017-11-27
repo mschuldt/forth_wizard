a074e625398dadf64950c08e8119d2569dbd767f
mschuldt <mbschuldt@gmail.com>
2017-11-26 21:39:30 -0800

    description and URLs

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from distutils.core import setup, Extension
 setup(name='forthwiz',
       packages=['forthwiz'],
       version='1.0',
+      description='Finds optimal forth code sequences for stack transformations',
+      url='https://github.com/mschuldt/forth_wizard',
+      download_url='https://github.com/mschuldt/forth_wizard/archive/1.0.tar.gz',
       author='Michael Schuldt',
       author_email='mbschuldt@gmail.com',
       ext_modules=[Extension('chuckmoore', ['forthwiz.c'])])
