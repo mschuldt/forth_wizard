7d7531a8e1cc4dfe819e64edd3d5edbf0b490246
mschuldt <mbschuldt@gmail.com>
2017-11-26 22:38:38 -0800

    enforce python3 during setup

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup, Extension
+import sys
+
+assert not sys.version_info.major < 3, 'The forth wizard requires python3'
+
+v='1.2'
 
 setup(name='forthwiz',
       packages=['forthwiz'],
-      version='1.1',
+      version=v,
       description='Finds optimal forth code sequences for stack transformations',
       url='https://github.com/mschuldt/forth_wizard',
-      download_url='https://github.com/mschuldt/forth_wizard/archive/1.1.tar.gz',
+      download_url='https://github.com/mschuldt/forth_wizard/archive/{}.tar.gz'.format(v),
       author='Michael Schuldt',
       author_email='mbschuldt@gmail.com',
       ext_modules=[Extension('chuckmoore', ['forthwiz.c'] )],
