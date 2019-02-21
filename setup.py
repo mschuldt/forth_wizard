c4dc0249a0944b082e644fc3b32a7b3defb85bda
mschuldt <mbschuldt@gmail.com>
2019-02-20 16:54:52 -0800

    version 2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup, Extension
 import sys
 
-version = '2.1' # also change in forthwiz.py
+version = '2.2' # also change in forthwiz.py
 
 assert not sys.version_info.major < 3, 'The forth wizard requires python3'
 
